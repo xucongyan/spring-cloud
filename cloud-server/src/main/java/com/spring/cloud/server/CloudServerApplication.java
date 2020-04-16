db74ce885f61277868db9d40f2cd7761d383ddba
xucongyan <838587329@qq.com>
2020-04-16 14:57:21 +0800

    1.添加熔断监控界面，并修改feign调用超时时间； 2.增加config-server模块，读取git上的配置文件

--- cloud-server/src/main/java/com/spring/cloud/server/CloudServerApplication.java
+++ cloud-server/src/main/java/com/spring/cloud/server/CloudServerApplication.java
@@ -3,12 +3,13 @@ package com.spring.cloud.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableFeignClients //这个注解是通知SpringBoot在启动的时候，扫描被 @FeignClient 修饰的类
-@EnableDiscoveryClient
-@SpringCloudApplication
+@SpringCloudApplication //组合三个注解的功能，包含注册eureka注册中心，开启熔断，成为spring Boot注解
+@EnableHystrixDashboard //开启熔断监控
 public class CloudServerApplication {
 
     public static void main(String[] args) {
---
+++ cloud-server/src/main/java/com/spring/cloud/server/config/HystrixDashboardConfig.java
@@ -0,0 +1,27 @@
+package com.spring.cloud.server.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xucongyan
+ */
+@Configuration
+public class HystrixDashboardConfig {
+
+    /**
+     * 自己手动注册一个servlet到容器中，开启hystrix监控
+     * @return 注册servletBean
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
