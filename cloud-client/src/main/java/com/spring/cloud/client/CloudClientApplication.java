22b41ea6f8eca283d443e81788836e1c3c666f2d
xucongyan <838587329@qq.com>
2020-04-21 13:29:28 +0800

    引入cache支持，采用redis的形式

--- cloud-client/src/main/java/com/spring/cloud/client/CloudClientApplication.java
+++ cloud-client/src/main/java/com/spring/cloud/client/CloudClientApplication.java
@@ -3,11 +3,13 @@ package com.spring.cloud.client;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.spring.cloud.client.dao")
 @EnableDiscoveryClient
+@EnableCaching
 public class CloudClientApplication {
 
     public static void main(String[] args) {
--- cloud-client/src/main/java/com/spring/cloud/client/service/impl/UserBizServiceImpl.java
+++ cloud-client/src/main/java/com/spring/cloud/client/service/impl/UserBizServiceImpl.java
@@ -1,6 +1,5 @@
 package com.spring.cloud.client.service.impl;
 
-import com.alibaba.fastjson.JSON;
 import com.spring.cloud.client.dao.UserDao;
 import com.spring.cloud.client.entity.User;
 import com.spring.cloud.client.service.UserBizService;
@@ -8,20 +7,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author xucongyan
  */
 @Service
+@CacheConfig(cacheNames = "users")
 public class UserBizServiceImpl implements UserBizService {
 
     private static Logger logger = LoggerFactory.getLogger(UserBizServiceImpl.class);
-
-    private final static String REDIS_KEY_PREFIX = "cloud_client_user";
 
     @Value("${spring.profiles.xcy}")
     private String hello;
@@ -29,29 +27,15 @@ public class UserBizServiceImpl implements UserBizService {
     @Autowired
     private UserDao userDao;
 
-    @Autowired
-    private StringRedisTemplate redisTemplate;
 
     @Override
+    @Cacheable(key = "#p0")
     public User getUserByUsername(String username) {
         logger.info("从config中心获取到的信息为:" + hello);
 
-        String key = REDIS_KEY_PREFIX + username;
+        User user = userDao.getUserByUsername(username);
 
-        User user;
-
-        String jsonString = redisTemplate.opsForValue().get(key);
-        if (null != jsonString) {
-            user = JSON.parseObject(jsonString, User.class);
-            logger.info("从缓存中获取到了信息" + jsonString);
-        } else {
-            user = userDao.getUserByUsername(username);
-            jsonString = JSON.toJSONString(user);
-            logger.info("将信息从数据库中读取" + jsonString);
-
-            redisTemplate.opsForValue().set(key, jsonString, 60, TimeUnit.SECONDS);
-            logger.info("将信息写入了缓存中" + jsonString);
-        }
+        logger.info("将信息从数据库中读取");
 
         return user;
     }
