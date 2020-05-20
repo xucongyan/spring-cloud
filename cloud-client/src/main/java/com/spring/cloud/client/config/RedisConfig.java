f842e1c4e867b3ee2d13dabd2895018809049f04
xucongyan <838587329@qq.com>
2020-05-20 17:16:54 +0800

    RedisConfig redis工具配置的添加 RedisUtil redis工具类的修改

---
+++ cloud-client/src/main/java/com/spring/cloud/client/config/RedisConfig.java
@@ -0,0 +1,52 @@
+package com.spring.cloud.client.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        //json解析器
+        Jackson2JsonRedisSerializer jsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        jsonRedisSerializer.setObjectMapper(objectMapper);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // key采用String的序列化方式
+
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+
+        // hash的key也采用String的序列化方式
+
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+
+        // value序列化方式采用jackson
+
+        redisTemplate.setValueSerializer(jsonRedisSerializer);
+
+        // hash的value序列化方式采用jackson
+        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
+
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+}
--- cloud-client/src/main/java/com/spring/cloud/client/util/RedisUtil.java
+++ cloud-client/src/main/java/com/spring/cloud/client/util/RedisUtil.java
@@ -1,10 +1,17 @@
 package com.spring.cloud.client.util;
 
+import com.spring.cloud.client.config.RedisConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.connection.ReturnType;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Component
@@ -14,7 +21,96 @@ public class RedisUtil {
     private boolean is_redis_open;
 
     @Autowired
-    private StringRedisTemplate stringRedisTemplate;
+    private RedisConfig redisConfig;
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+
+    /**
+     * 尝试获取分布式锁
+     *
+     * @param key      key
+     * @param value    value
+     * @param expire   失效时间
+     * @param timeUnit 时间单位
+     * @return 是否锁定成功
+     */
+    public synchronized boolean lock(String key, String value, long expire, TimeUnit timeUnit) {
+        return (boolean) redisTemplate.execute((RedisCallback) connection -> {
+            return connection.set(key.getBytes(), value.getBytes(), Expiration.from(expire, timeUnit), RedisStringCommands.SetOption.SET_IF_ABSENT);
+        });
+    }
+
+    /**
+     * 尝试释放分布式锁
+     *
+     * @param key   key
+     * @param value value
+     * @return 是否释放成功
+     */
+    public synchronized boolean releaseLock(String key, String value) {
+        return (boolean) redisTemplate.execute((RedisCallback) connection -> {
+            String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+            return connection.eval(script.getBytes(), ReturnType.BOOLEAN, 1, key.getBytes(),
+                    value.getBytes());
+        });
+    }
+
+    /**
+     * 指定key缓存失效时间
+     *
+     * @param key      key
+     * @param expire   失效时间
+     * @param timeUnit 时间单位
+     * @return 是否设置成功
+     */
+    public boolean expire(String key, long expire, TimeUnit timeUnit) {
+        if (expire > 0) {
+            redisTemplate.expire(key, expire, timeUnit);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 获取key的失效时间
+     *
+     * @param key key
+     * @return 失效时间，返回0代表永久有效
+     */
+    public long getExpire(String key) {
+        return redisTemplate.getExpire(key);
+    }
+
+    /**
+     * 判断缓存中是否含有key
+     *
+     * @param key key
+     * @return 判断结果
+     */
+    public boolean hashKey(String key) {
+        return redisTemplate.hasKey(key);
+    }
+
+    /**
+     * 删除一个或者多个key
+     *
+     * @param key key
+     * @return 是否删除成功
+     */
+    public boolean delete(String... key) {
+        if (key != null && key.length > 0) {
+            if (key.length == 1) {
+                return redisTemplate.delete(key[0]);
+            } else {
+                Long result = redisTemplate.delete(CollectionUtils.arrayToList(key));
+                return key.length == result;
+            }
+        }
+        return false;
+    }
+
 
     /**
      * 从Redis中获取信息
@@ -22,11 +118,8 @@ public class RedisUtil {
      * @param key Redis的key
      * @return 获取到的值
      */
-    public String getValueByKey(String key) {
-        if (is_redis_open) {
-            return stringRedisTemplate.opsForValue().get(key);
-        }
-        return null;
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
     }
 
     /**
@@ -35,21 +128,10 @@ public class RedisUtil {
      * @param key   Redis的key
      * @param value Redis的value
      */
-    public void setValue(String key, String value) {
-        if (is_redis_open) {
-            stringRedisTemplate.opsForValue().set(key, value);
-        }
+    public void set(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
     }
 
-    /**
-     * 使用默认的超时时间设置Redis的key和value
-     *
-     * @param key   Redis的key
-     * @param value Redis的value
-     */
-    public void setValueByDefaultTime(String key, String value) {
-        setValue(key, value, 60, TimeUnit.SECONDS);
-    }
 
     /**
      * 使用自定义的超时时间设置Redis的key和value
@@ -59,9 +141,54 @@ public class RedisUtil {
      * @param timeout 超时时间
      * @param unit    时间单位
      */
-    public void setValue(String key, String value, long timeout, TimeUnit unit) {
-        if (is_redis_open) {
-            stringRedisTemplate.opsForValue().set(key, value, timeout, unit);
+    public void set(String key, String value, long timeout, TimeUnit unit) {
+        if (timeout > 0) {
+            redisTemplate.opsForValue().set(key, value, timeout, unit);
+        } else {
+            this.set(key, value);
         }
+    }
+
+    /**
+     * 递增
+     *
+     * @param key   key
+     * @param delta 需要递增的大小
+     * @return 执行递增之后的结果
+     */
+    public long incr(String key, long delta) {
+        return redisTemplate.opsForValue().increment(key, delta);
+    }
+
+    /**
+     * 递减
+     *
+     * @param key   key
+     * @param delta 需要递减的大小
+     * @return 执行递减之后的结果
+     */
+    public long decr(String key, long delta) {
+        return redisTemplate.opsForValue().increment(key, -delta);
+    }
+
+    /**
+     * 获取hash中的值
+     *
+     * @param key     key
+     * @param hashKey hashKey
+     * @return 获取到的值
+     */
+    public Object hGet(String key, String hashKey) {
+        return redisTemplate.opsForHash().get(key, hashKey);
+    }
+
+    /**
+     * 获取RedisKey所有的hash键值对
+     *
+     * @param key Redis的key
+     * @return 获取到的值
+     */
+    public Map<Object, Object> hashGet(String key) {
+        return redisTemplate.opsForHash().entries(key);
     }
 }
